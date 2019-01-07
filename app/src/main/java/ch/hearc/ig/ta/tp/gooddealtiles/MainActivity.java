f5d29a824f2f91cd2b82ef7962db4520e611914a
eyreko <lou.simon@hotmail.com>
2019-01-07 14:19:33 +0100

    message in the second page is based on the id of the tile

--- app/src/main/java/ch/hearc/ig/ta/tp/gooddealtiles/MainActivity.java
+++ app/src/main/java/ch/hearc/ig/ta/tp/gooddealtiles/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "fdafafda";
+    public static final String EXTRA_MESSAGE = "DealText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayTileActivity.class);
-        String message = "dgfdgdg";
+        String message = "?";
+        switch (view.getId()) {
+            case (R.id.tile1):
+                message = "Payot 10% de réduction !";
+                Log.d("GoodDealTiles","tile1");
+                break;
+            case (R.id.tile2):
+                message = "Billet à l'étranger 50% avec CFF Railplus";
+                Log.d("GoodDealTiles","tile2");
+                break;
+        }
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-        Log.d("GoodDealTiles","hello");
+
     }
 }
